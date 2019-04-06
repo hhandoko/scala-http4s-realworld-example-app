3f2c5bdb2d915fdefea66c65c5722342b0b61cb5
Herdy Handoko <herdy.handoko@gmail.com>
2019-04-06 15:32:01 +0800

    Remove `Joke` example

--- src/main/scala/com/hhandoko/realworld/RealworldServer.scala
+++ src/main/scala/com/hhandoko/realworld/RealworldServer.scala
@@ -1,11 +1,7 @@
 package com.hhandoko.realworld
 
-import scala.concurrent.ExecutionContext.global
-
 import cats.effect.{ConcurrentEffect, ContextShift, Timer}
-import cats.implicits._
 import fs2.Stream
-import org.http4s.client.blaze.BlazeClientBuilder
 import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.server.middleware.Logger
@@ -15,27 +11,16 @@ import com.hhandoko.realworld.tag.{TagRoutes, TagService}
 object RealworldServer {
 
   def stream[F[_]: ConcurrentEffect: ContextShift](implicit T: Timer[F]): Stream[F, Nothing] = {
+    val tagService   = TagService.impl[F]
+    val httpApp      = TagRoutes[F](tagService).orNotFound
+    val finalHttpApp = Logger.httpApp(logHeaders = true, logBody = true)(httpApp)
+
     for {
-      client        <- BlazeClientBuilder[F](global).stream
-      jokeAlg        = Jokes.impl[F](client)
-      tagService     = TagService.impl[F]
-
-      // Combine Service Routes into an HttpApp.
-      // Can also be done via a Router if you
-      // want to extract a segments not checked
-      // in the underlying routes.
-      httpApp = (
-        RealworldRoutes.jokeRoutes[F](jokeAlg) <+>
-        TagRoutes[F](tagService)
-      ).orNotFound
-
-      // With Middlewares in place
-      finalHttpApp = Logger.httpApp(logHeaders = true, logBody = true)(httpApp)
-
       exitCode <- BlazeServerBuilder[F]
         .bindHttp(8080, "0.0.0.0")
         .withHttpApp(finalHttpApp)
         .serve
     } yield exitCode
   }.drain
+
 }
