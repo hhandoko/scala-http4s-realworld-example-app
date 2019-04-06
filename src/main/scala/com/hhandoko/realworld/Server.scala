e58baaa553218e7516bb374c6e96830b66723bb2
Herdy Handoko <herdy.handoko@gmail.com>
2019-04-06 15:41:10 +0800

    Parameterise host and port (to environment variable)

--- src/main/scala/com/hhandoko/realworld/Server.scala
+++ src/main/scala/com/hhandoko/realworld/Server.scala
@@ -11,12 +11,15 @@ import com.hhandoko.realworld.tag.{TagRoutes, TagService}
 object Server {
 
   def stream[F[_]: ConcurrentEffect: ContextShift: Timer]: Stream[F, ExitCode] = {
+    val host = Option(System.getenv("APP_HOST")).getOrElse("0.0.0.0")
+    val port = Option(System.getenv("APP_PORT")).map(_.toInt).getOrElse(8080)
+
     val tagService = TagService.impl[F]
     val routes     = TagRoutes[F](tagService).orNotFound
     val httpApp    = Logger.httpApp(logHeaders = true, logBody = true)(routes)
 
     BlazeServerBuilder[F]
-      .bindHttp(8080, "0.0.0.0")
+      .bindHttp(port, host)
       .withHttpApp(httpApp)
       .serve
   }
