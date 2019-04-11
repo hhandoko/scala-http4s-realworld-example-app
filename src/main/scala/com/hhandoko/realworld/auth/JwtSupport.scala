f80cce9f10be3e25939e3bc5baa6c08fc804791f
Herdy Handoko <herdy.handoko@gmail.com>
2019-04-11 19:32:51 +0800

    Use fake JWT implementation due to Graal native-image issue

--- src/main/scala/com/hhandoko/realworld/auth/JwtSupport.scala
+++ src/main/scala/com/hhandoko/realworld/auth/JwtSupport.scala
@@ -1,8 +1,10 @@
 package com.hhandoko.realworld.auth
 
-import java.time.Instant
+//import java.time.Instant
+//
+//import pdi.jwt.{JwtAlgorithm, JwtCirce, JwtClaim}
 
-import pdi.jwt.{JwtAlgorithm, JwtCirce, JwtClaim}
+import pdi.jwt.JwtAlgorithm
 
 import com.hhandoko.realworld.core.JwtToken
 
@@ -14,12 +16,15 @@ trait JwtSupport {
 
   def generateToken(): JwtToken = {
     // TODO: Make expiration configurable, and use Scala time classes
-    val claim = JwtClaim(
-      expiration = Some(Instant.now.plusSeconds(157784760).getEpochSecond),
-      issuedAt = Some(Instant.now.getEpochSecond)
-    )
-
-    JwtToken(JwtCirce.encode(claim, SECRET, ALGO))
+    // FIXME: Graal native-image throws runtime error
+    // See: https://github.com/oracle/graal/issues/1152
+//    val claim = JwtClaim(
+//      expiration = Some(Instant.now.plusSeconds(157784760).getEpochSecond),
+//      issuedAt = Some(Instant.now.getEpochSecond)
+//    )
+//
+//    JwtToken(JwtCirce.encode(claim, SECRET, ALGO))
+    JwtToken("some.fake.token")
   }
 
 }
