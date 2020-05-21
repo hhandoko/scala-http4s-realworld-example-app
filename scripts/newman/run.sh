d58d8250914c04eb868c06f1924cccf5c2201b4e
Herdy Handoko <herdy.handoko@gmail.com>
2020-05-21 16:26:32 +0800

    Add Postman collection tests via `newman`

---
+++ scripts/newman/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+APIURL=${APIURL:-http://localhost:8080/api}
+USERNAME=${USERNAME:-john.doe}
+EMAIL=${EMAIL:-$USERNAME@test.com}
+PASSWORD=${PASSWORD:-S3cret!}
+
+newman run scripts/newman/Conduit.postman_collection.json \
+  --delay-request 100 \
+  --global-var "APIURL=$APIURL" \
+  --global-var "USERNAME=$USERNAME" \
+  --global-var "EMAIL=$EMAIL" \
+  --global-var "PASSWORD=$PASSWORD"
